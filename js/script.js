7767a3f09fe468ac097672b80b95431a82894d09
Method <84025863+Method92@users.noreply.github.com>
2022-03-30 17:13:06 +0100

    Code Clean up. JSON examples included

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  var playerCount=0;
+  var playerCount = 0;
 
   $('#playerCount').on("change", showPlayerCount);
   $('#playButton').on("click", loadNameInput)
 
-  function showPlayerCount(){
+  function showPlayerCount() {
     playerCount = document.getElementById('playerCount').value;
     var outputMessage = "You have selected " + playerCount + " players. Press play to start";
     document.getElementById('playerOutput').innerHTML = outputMessage;
     $('#playButton').show();
   }
 
-  function loadNameInput(){
+  function loadNameInput() {
     var mainSection = document.getElementById("mainSection");
-    mainSection.innerHTML ="<h2> Please enter player names</h2>";
-    for (let i = 0; i<playerCount; i++) {
-      mainSection.innerHTML += "<p> Player " + (i+1) + "'s name:</p>";
-      mainSection.innerHTML += ("<input type='text' name='Player " + (i+1) + "' placeholder='Player " + (i+1) + " name..' name id='player"+ (i+1) +"name'>");
+    mainSection.innerHTML = "<h2> Please enter player names</h2>";
+    for (let i = 0; i < playerCount; i++) {
+      mainSection.innerHTML += "<p> Player " + (i + 1) + "'s name:</p>";
+      mainSection.innerHTML += ("<input type='text' name='Player " + (i + 1) + "' placeholder='Player " + (i + 1) + " name..' name id='player" + (i + 1) + "name'>");
       mainSection.innerHTML += "<br>"
     }
   };
