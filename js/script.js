6d73b01310b9d9ada22185dc514c2b708e11c6d0
Method <84025863+Method92@users.noreply.github.com>
2022-04-13 16:06:25 +0100

    fixed naming progression, still lacking functionality

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
 $(document).ready(function() {
 
   var playerCount = 0;
-  $('#playerCount').on("change", showPlayerCount);
-  $('#playButton').on("click", loadNameInput)
-
-  function showPlayerCount() {
-    playerCount = document.getElementById('playerCount').value;
-    var outputMessage = "You have selected " + playerCount + " players. Press play to start";
-    document.getElementById('playerOutput').innerHTML = outputMessage;
-    $('#playButton').show();
-  }
-
-  function loadNameInput() {
-    var workingSection = document.getElementById("workingSection");
-    workingSection.innerHTML = "<h2> Please enter player names</h2>";
-    for (let i = 0; i < playerCount; i++) {
-      workingSection.innerHTML += "<p> Player " + (i + 1) + "'s name:</p>";
-      workingSection.innerHTML += ("<input type='text' name='Player " + (i + 1) + "' placeholder='Player " + (i + 1) + " name..' name id='player" + (i + 1) + "name'>");
-      workingSection.innerHTML += "<br>"
-    }
-  };
+  $('#inputPlayerCount').on("change", processPlayerCount);
+  $('#startNamingButton').on("click", loadNameInput)
 
 });
+
+function processPlayerCount() {
+  playerCount = document.getElementById("inputPlayerCount").value;
+  var outputMessage = "You have selected " + playerCount + " players. Press play to start";
+  document.getElementById('outputSection').innerHTML = outputMessage;
+  $('#startNamingButton').show();
+}
+
+function loadNameInput() {
+  $("#playerNumberSection").hide();
+  $("#playerNamingSection").show();
+  playerCountSection.innerHTML = "<h2> Please enter player names</h2>";
+}
