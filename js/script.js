76b73ebbffbb2d35aed51fb1d1c84ac847e126cf
Method <84025863+Method92@users.noreply.github.com>
2022-04-13 19:06:22 +0100

    created functionality on name insertion
    saved all names into array and printed welcome message

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
+let players = [];
 
-  let playerCount = 0;
-  let players = ["Phil", "Jim", "Tina"];
+$(document).ready(function () {
   $('#playerCountInput').on("change", processPlayerCount);
   $('#startNamingButton').on("click", loadNameInput);
-  $('#startPlayButton').on("click", function() {processNames(playerCount, players), welcomePlayers});
+  $('#continueButton').on("click", function() {processNames(players)});
 });
 
 function processPlayerCount() {
+  playerCount = $('#playerCountInput').val();
   playerCount = document.getElementById("playerCountInput").value;
   $('#outputSection').html("You have selected " + playerCount + " players. Press 'Go!' to start");
   $('#startNamingButton').show();
@@ -15,7 +15,7 @@ function processPlayerCount() {
 
 function loadNameInput() {
   $("#outputSection").html("Please ensure all names are entered and press 'Play' to begin >> " + playerCount);
-  var inputFieldString = "";
+  let inputFieldString = "";
   for(let i = 0; i < playerCount; i++){
     inputFieldString += "<input type=\"text\" class=\"nameInput\" id=\"player"+(i+1)+"Name\" placeholder=\"Player "+(i+1)+"'s name..\" />";
   }
@@ -25,22 +25,35 @@ function loadNameInput() {
 }
 
 function welcomePlayers(){
-
-  // $('#playerNamingSection').hide();
-  // $('#welcomeSection').show();
-  // let welcomeString = "";
-  // for(let i = 0; i < players.length; i++){
-  //       welcomeString += "welcome " + players[(i+1)];
-  // }
-  // $('#welcomeForm').html(welcomeString);
+  $('#playerNamingSection').hide();
+  $('#welcomeSection').show();
+  let welcomeString = "";
+  for(let i = 0; i < players.length; i++){
+        welcomeString += "welcome " + players[(i+1)];
+  }
+  $('#welcomeForm').html(welcomeString);
 }
 
 function processNames(){
-  let count = arguments[0];
-  console.log("count " + count);
-  for(let i = 0; i < count; i++){
-    //let playerName = $('#player'+(i+1)+'Name').val();
-    //players.push(arguments[i]);
-    console.log(arguments[1][i]);
+  $('#playerNamingSection').hide();
+  $('#welcomeSection').show();
+
+  for(let i = 0; i < playerCount; i++){
+    let playerName = $('#player'+(i+1)+'Name').val();
+    arguments[0].push(playerName);
+    console.log("pushing " + playerName);
   }
+  console.log("Array after push: " + arguments[0] + ". Length: " + arguments[0].length);
+
+  let welcomeString = "Welcome to the game ";
+  for(let i = 0; i < players.length; i++){
+    if(i === (players.length-1)){
+      welcomeString += " and " + players[i];
+    }
+    else{
+      welcomeString += players[i] + ", ";
+    }
+  }
+  $('#welcomeForm').html(welcomeString);
+  $('#outputSection').html("Press 'play' to begin!")
 }
