042c28d498ef8512fee72f060270149d06f415df
Method <84025863+Method92@users.noreply.github.com>
2022-04-28 17:27:45 +0100

    refactored code

--- js/script.js
+++ js/script.js
@@ -4,22 +4,44 @@ let currentPlayerIndex = -1;
 let currentPlayer = "";  
 
 $(document).ready(function () {
+  //Starting screen 
   $('#playerCountInput').on("change", showPlayerCount);
-  $('#playerCountButton').on("click", loadNameInput);
+  $('#playerCountButton').on("click", function(){
+    loadNameInput();
+    $('#playerCountSection').hide();
+    $('#playerNamingSection').show();
+  });
+//Saving names, determining roles and welcoming players. 
   $('#playerNamingButton').on("click", function () {
     processNames(players);
     [murdererIndex, lawyerIndex] = determineRoles(players);
-    welcomePlayers(players)
+    welcomePlayers(players);
+    $('#playerNamingSection').hide();
+    $('#welcomeSection').show();
   });
+//Determining next player, asking for ready check. 
   $('.continuePlayButton').on("click", function () {
+    $('#welcomeSection').hide(); 
+    $('#roleSection').show();
+    $('.continuePlayButton').html("OK");
+    $('.outputArea').html("");
     determinePlayer(currentPlayer);
-    $('.continuePlayButton').html("continue");
-    $('#outputSection').html("");
+    roleReadyCheck();
   }); 
-  $('#revealButton').on("click", function () {
+//Showing role to player.
+  $('#revealRoleButton').on("click", function () {
     showRole();
   });
-  $('.questionButton').on("click", askQuestion);
+//Determining who is next, 
+  $('#completeRolesButton').on("click", function(){
+    determinePlayer();
+    questionReadyCheck();
+    $('#questionSection').show();
+    $('#roleSection').hide();
+  });
+  $('#questionButton').on("click", function(){
+    askQuestion();
+  });
 });
 
 //Shows the players how many players have been selected, shows hidden button to continue
@@ -30,8 +52,7 @@ function showPlayerCount() {
   $('#playerCountButton').show();
 }
 
-//Determies how many input fields are required (based on player count), inserts as html
-//hides 'playerCountingSection, shows playerNaming section
+//Determies how many input fields are required (based on player count), inserts as html to playerNamingArea
 function loadNameInput() {
   $("#outputSection").html("Please ensure all names are entered and press 'start' when ready >> <br />  " + playerCount);
   let inputFieldString = "";
@@ -39,8 +60,6 @@ function loadNameInput() {
     inputFieldString += "<input type=\"text\" class=\"nameInput\" id=\"player" + (i + 1) + "Name\" placeholder=\"Player " + (i + 1) + "'s name..\" />";
   }
   $('#playerNamingArea').html(inputFieldString);
-  $('#playerCountSection').hide();
-  $('#playerNamingSection').show();
 }
 
 //Saves user-supplied names into 'players' array
@@ -65,13 +84,9 @@ function determineRoles(arr) {
   return [randomNumber, secondRandomNumber];
 }
 
-//Hides naming section, shows welcome section. 
 //Welcomes players based on names supplied - initially used for testing purposes
 function welcomePlayers() {
-  $('#playerNamingSection').hide();
-  $('#welcomeSection').show();
-
-  let welcomeString = "Welcome to the game ";
+  let welcomeString = "";
   for (let i = 0; i < players.length; i++) {
     if (i === (players.length - 1)) {
       welcomeString += " and " + players[i];
@@ -80,47 +95,62 @@ function welcomePlayers() {
     }
   }
   $('#welcomeArea').html(welcomeString);
-  $('#outputSection').html("Press 'play' to begin! **Murderer = " + players[murdererIndex] + " lawyer = " + players[lawyerIndex] + "**")
+  $('.outputArea').html("Press 'Start' to begin! **Murderer = " + players[murdererIndex] + " lawyer = " + players[lawyerIndex] + "**")
   $('.continuePlayButton').show(); 
 
 }
 
+//Determines who is next in the player queue and asks them to click the button when ready
 function determinePlayer() {
-  $('#welcomeSection').hide(); 
-  $('#playSection').show();
   currentPlayerIndex = (currentPlayerIndex + 1) % players.length; 
   currentPlayer = players[currentPlayerIndex]; 
-  $('#playArea').html(currentPlayer + ", it's your turn! Make sure only you can see the screen and press the button."); 
-  $('.continuePlayButton').hide();
-  $('#revealButton').html("I'm " + currentPlayer);
-  $('#revealButton').show();
 }
 
+//Ready screen for role revealing 
+function roleReadyCheck(){
+  $('#roleArea').html(currentPlayer + ", it's your turn! Make sure only you can see the screen and press the button."); 
+  $('.continuePlayButton').hide();
+  $('#revealRoleButton').html("I'm " + currentPlayer);
+  $('#revealRoleButton').show();
+}
+
+//Ready screen for question asking
+function questionReadyCheck(){
+  $('#questionArea').html(currentPlayer + ", time for a question. Gain control and press the button."); 
+  $('#completeRolesButton').hide();
+  $('#questionButton').html("I'm " + currentPlayer);
+  $('#questionButton').show();
+}
+
+//Reveals whether player is the lawyer, the killer, or a regular player
 function showRole() {
   console.log(currentPlayerIndex + "<-- current player Index");
 
   if(currentPlayerIndex === murdererIndex){
-    $('#playArea').html("You are the murderer! Try to guess your way through the questions and avoid detection"); 
-    $('#revealButton').html("continue");
-  } else if(currentPlayerIndex === lawyerIndex){
-      $('#playArea').html("You are the lawyer! Try to guess who the murderer is and protect them! Hint: the word is 'fish'"); 
-      $('#revealButton').html("continue");
-  } else { 
-    $('#playArea').html("The word is fish."); 
-    $('#revealButton').html("continue");
+    $('#roleArea').html("You are the murderer! Try to guess your way through the questions and avoid detection"); 
+  } 
+  else if(currentPlayerIndex === lawyerIndex){
+      $('#roleArea').html("You are the lawyer! Try to guess who the murderer is and protect them! Hint: the word is 'fish'"); 
+  } 
+  else { 
+    $('#roleArea').html("The word is fish."); 
   }
-  $('#revealButton').hide();
+  $('#revealRoleButton').hide();
   if (currentPlayerIndex === (players.length-1)){
-    $('.questionButton').show();
+    $('#completeRolesButton').show();
   } else { 
     $('.continuePlayButton').show();
   }
 }
 
+//Asks question --- *** need to create list of questions relevant to category - find out how to read from XML file ***
+//Should read from XML file. 
 function askQuestion(){
-  $('#playSection').hide(); 
+  $('#roleSection').hide(); 
   $('#questionSection').show(); 
   $('.questionButton').hide();
+
+  let question = "What is the meaning of life?";
 }
 
 // ** For use if needed - used for debugging **
